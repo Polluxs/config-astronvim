ff154802b8460bb35abcef1d6ce3158864d96760
Polluxs <thomasdorissen@gmail.com>
2024-07-04 00:10:58 +0200

    Add auto-save

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python" },
