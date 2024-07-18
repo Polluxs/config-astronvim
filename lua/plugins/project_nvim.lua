b5af099055461ea29ea412256c424bb33261a884
Polluxs <thomasdorissen@gmail.com>
2024-07-18 10:31:07 +0200

    Automatically swith workspace directory when switching projects

--- lua/plugins/project_nvim.lua
+++ lua/plugins/project_nvim.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     projects = { -- define project roots
       "~/code/vrg/*",
+      "~/code/vrg/anp/python/*",
+      "~/code/vrg/anp/go/*",
       "~/code/pollywolly.io/*",
       "~/code/time_hacking/*",
       "~/code/templates/*",
@@ -12,6 +14,12 @@ return {
   init = function()
     -- enable saving the state of plugins in the session
     vim.opt.sessionoptions:append "globals" -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+
+    -- Automatically change the workspace based on the defined project roots
+    require("neovim-project").setup {
+      patterns = { ".git", "Makefile", "package.json" }, -- Patterns to detect project root
+      silent_chdir = true, -- Change directory silently
+    }
   end,
   dependencies = {
     { "nvim-lua/plenary.nvim" },
