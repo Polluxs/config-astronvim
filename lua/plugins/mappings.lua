94c8c794b55b42884b28d347265802843bbe5697
Polluxs <thomasdorissen@gmail.com>
2024-09-25 15:51:34 +0200

    Add chatGPT && refactoring of the PrimeTime

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -27,9 +27,45 @@ return {
           ["<Leader>ml"] = { "<cmd>DBUILastQuery<cr>", desc = "Show Last Query" },
 
           -- Extend NeoTest
-          ["<leader>Tl"] = {
+          ["<leader>tl"] = {
             function() require("neotest").run.run_last { strategy = "dap" } end,
             desc = "Debug last test",
+          },
+
+          -- ChatGPT mappings
+          ["<Leader>c"] = { "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+          ["<Leader>ce"] = { "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+          ["<Leader>cg"] = { "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+          ["<Leader>ct"] = { "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+          ["<Leader>ck"] = { "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+          ["<Leader>cd"] = { "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+          ["<Leader>ca"] = { "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+          ["<Leader>co"] = { "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+          ["<Leader>cs"] = { "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+          ["<Leader>cf"] = { "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+          ["<Leader>cx"] = { "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
+          ["<Leader>cr"] = { "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+          ["<Leader>cl"] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", desc = "Code Readability Analysis" },
+          ["<F6>"] = {
+            function()
+              local chat_window_open = vim.fn.winnr "$" > 1 -- Check if more than one window is open
+              if chat_window_open then
+                vim.cmd "ChatGPT" -- Close the ChatGPT window (only works in selection mode)
+              else
+                vim.cmd "ChatGPT" -- Open the ChatGPT window
+              end
+            end,
+            desc = "Toggle ChatGPT",
+          },
+        },
+        x = {
+          ["<leader>re"] = { ":Refactor extract<cr>", desc = "Extract Function" },
+          ["<leader>rf"] = { ":Refactor extract_to_file<cr>", desc = "Extract Function To File" },
+          ["<leader>rv"] = { ":Refactor extract_var<cr>", desc = "Extract Variable" },
+          ["<leader>ri"] = { ":Refactor inline_var<cr>", desc = "Inline Variable" },
+          ["<leader>rr"] = {
+            function() require("telescope").extensions.refactoring.refactors() end,
+            desc = "Refactoring options",
           },
         },
         t = {
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
   "folke/tokyonight.nvim",
   name = "tokyonight",
   opts = {
-    style = "night", -- Choose between 'storm', 'night', and 'day'
+    style = "storm", -- Choose between 'storm', 'night', and 'day'
     transparent = false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
@@ -16,41 +16,41 @@ return {
     plugins = {},
     -- Override specific colors here
     on_colors = function(colors)
-      colors.info = "#e39552" -- TODO
-      colors.magenta = "#f5646a" -- git accept
-      colors.blue = "#f07176" -- Method name
-      colors.bg = "#171821" -- Background color
-      colors.bg_dark = "#1e2030" -- Darker background
-      colors.bg_highlight = "#1f2130" -- Highlighted background
-      colors.blue0 = "#912fc2" -- Replaced with purple
-      colors.blue1 = "#f78460" -- Replaced with purple
-      colors.blue2 = "#5d04c2" -- Replaced with purple
-      colors.blue5 = "#d1a6f0" -- Replaced with purple
-      colors.blue6 = "#e6a7f0" -- Replaced with purple
-      colors.blue7 = "#6a2f6e" -- Replaced with purple
-      colors.comment = "#636da6" -- Comment color
-      colors.cyan = "#ffa07d" -- import variable color
-      colors.dark3 = "#545c7e"
-      colors.dark5 = "#737aa2"
-      colors.fg = "#c8d3f5" -- Foreground color
-      colors.fg_dark = "#828bb8" -- Darker foreground
-      colors.fg_gutter = "#3b4261" -- Gutter foreground
-      colors.green = "#ff966c" -- Replaced with orange
-      colors.green1 = "#ffc085" -- Replaced with orange
-      colors.green2 = "#ff9a3b" -- Replaced with orange
-      colors.magenta2 = "#ff007c"
-      colors.orange = "#ff966c" -- Orange color
-      colors.purple = "#f76a7d" -- for, if else, ...
-      colors.red = "#ff757f" -- Red color
-      colors.red1 = "#c53b53"
-      colors.teal = "#c53b53" -- Teal color
-      colors.terminal_black = "#444a73" -- Terminal black
-      colors.yellow = "#ffc777" -- Yellow color
-      colors.git = {
-        add = "#b8db87", -- Git add color
-        change = "#7ca1f2", -- Git change color
-        delete = "#e26a75", -- Git delete color
-      }
+      -- colors.info = "#e39552" -- TODO
+      -- colors.magenta = "#f5646a" -- git accept
+      -- colors.blue = "#f07176" -- Method name
+      -- colors.bg = "#171821" -- Background color
+      -- colors.bg_dark = "#1e2030" -- Darker background
+      -- colors.bg_highlight = "#1f2130" -- Highlighted background
+      -- colors.blue0 = "#912fc2" -- Replaced with purple
+      -- colors.blue1 = "#f78460" -- Replaced with purple
+      -- colors.blue2 = "#5d04c2" -- Replaced with purple
+      -- colors.blue5 = "#d1a6f0" -- Replaced with purple
+      -- colors.blue6 = "#e6a7f0" -- Replaced with purple
+      -- colors.blue7 = "#6a2f6e" -- Replaced with purple
+      -- colors.comment = "#636da6" -- Comment color
+      -- colors.cyan = "#ffa07d" -- import variable color
+      -- colors.dark3 = "#545c7e"
+      -- colors.dark5 = "#737aa2"
+      -- colors.fg = "#c8d3f5" -- Foreground color
+      -- colors.fg_dark = "#828bb8" -- Darker foreground
+      -- colors.fg_gutter = "#3b4261" -- Gutter foreground
+      -- colors.green = "#ff966c" -- Replaced with orange
+      -- colors.green1 = "#ffc085" -- Replaced with orange
+      -- colors.green2 = "#ff9a3b" -- Replaced with orange
+      -- colors.magenta2 = "#ff007c"
+      -- colors.orange = "#ff966c" -- Orange color
+      -- colors.purple = "#f76a7d" -- for, if else, ...
+      -- colors.red = "#ff757f" -- Red color
+      -- colors.red1 = "#c53b53"
+      -- colors.teal = "#c53b53" -- Teal color
+      -- colors.terminal_black = "#444a73" -- Terminal black
+      -- colors.yellow = "#ffc777" -- Yellow color
+      -- colors.git = {
+      --   add = "#b8db87", -- Git add color
+      --   change = "#7ca1f2", -- Git change color
+      --   delete = "#e26a75", -- Git delete color
+      -- }
     end,
   },
 }
