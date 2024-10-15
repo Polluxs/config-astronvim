c9d3174cdefd0e7911b3e93eebd5e0cf92aef457
Polluxs <thomasdorissen@gmail.com>
2024-10-15 15:18:14 +0200

    Improve chatgpt usage

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -3,7 +3,16 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup {
-      api_key_cmd = "gopass show -o -n /vrg/chatgpt",
+      api_key_cmd = "gopass show -o -n /vrg/chatgpt-legacy",
+      openai_params = {
+        max_tokens = 16000,
+        model = "gpt-4o-mini",
+        frequency_penalty = 0, -- allow it to repeat itself (0 = no penalty)
+        presence_penalty = 0, -- allow it to talk about new topic (0 = no penality to talk about new subjects)
+        temperature = 0.2,
+        top_p = 0.1, -- top_p = 0.1 means the model only considers the top 10% of possible word choices. Lower values make the responses more focused and deterministic.
+        n = 1, -- amount of responses (only 1)
+      },
     }
   end,
   dependencies = {
